5970f4081c72297388d4bb1c8107965d75b75419
Hanum Lee <daniel@Hanums-MacBook-Pro.local>
2018-10-08 20:46:01 -0600

    basic function added

--- Assignment1/decryptFile.java
+++ Assignment1/decryptFile.java
@@ -48,10 +48,81 @@ public class decryptFile{
         }catch (Exception e){
 
         }finally{
-            
+            if (in_file != null){
+				in_file.close();
+			}
+			if(out_file != null){
+				out_file.close();
+			}
         }
 
 
         System.out.println("decrypt worked");
     }
+
+    public static byte[] sha1_hash(byte[] input_data) throws Exception{
+		byte[] hashval = null;
+		try{
+			//create message digest object
+			MessageDigest sha1 = MessageDigest.getInstance("SHA1");
+			
+			//make message digest
+			hashval = sha1.digest(input_data);
+		}
+		catch(NoSuchAlgorithmException nsae){
+			System.out.println(nsae);
+		}
+		return hashval;
+    }
+    
+    public static String aes_decrypt(byte[] data_in) throws Exception{
+		byte[] decrypted = null;
+		String dec_str = null;
+		try{
+			//set cipher to decrypt mode
+			sec_cipher.init(Cipher.DECRYPT_MODE, sec_key_spec);
+
+			//do decryption
+			decrypted = sec_cipher.doFinal(data_in);
+
+			//convert to string
+			dec_str = new String(decrypted);
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+		return dec_str;
+    }
+    
+
+    /*
+     * Converts a byte array to hex string
+     * this code from http://java.sun.com/j2se/1.4.2/docs/guide/security/jce/JCERefGuide.html#HmacEx
+     */
+    public static String toHexString(byte[] block) {
+        StringBuffer buf = new StringBuffer();
+
+        int len = block.length;
+
+        for (int i = 0; i < len; i++) {
+             byte2hex(block[i], buf);
+             if (i < len-1) {
+                 buf.append(":");
+             }
+        } 
+        return buf.toString();
+    }
+    /*
+     * Converts a byte to hex digit and writes to the supplied buffer
+     * this code from http://java.sun.com/j2se/1.4.2/docs/guide/security/jce/JCERefGuide.html#HmacEx
+     */
+    public static void byte2hex(byte b, StringBuffer buf) {
+        char[] hexChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8',
+                            '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        int high = ((b & 0xf0) >> 4);
+        int low = (b & 0x0f);
+        buf.append(hexChars[high]);
+        buf.append(hexChars[low]);
+    }
+
 }
--- Assignment1/secureFile.java
+++ Assignment1/secureFile.java
@@ -40,13 +40,105 @@ public class secureFile{
         boolean verify = false;
         
         try{
+            in_file = new FileInputStream(args[0]);
+            out_file = new FileOutputStream(args[1]);
+            
+            byte[] msg = new byte[in_file.available()];
+			read_bytes = in_file.read(msg);
+
+            sha_hash = sha1_hash(msg);
+
+			//print out hash in hex
+            System.out.println("SHA-1 Hash: " + toHexString(sha_hash));
+            
+            //encrypt file with AES
+			//key setup - generate 128 bit key
+			key_gen = KeyGenerator.getInstance("AES");
+			key_gen.init(128);
+			sec_key = key_gen.generateKey();
+
+			//get key material in raw form
+			raw = sec_key.getEncoded();
+			sec_key_spec = new SecretKeySpec(raw, "AES");
+
+			//create the cipher object that uses AES as the algorithm
+			sec_cipher = Cipher.getInstance("AES");	
+
+			//do AES encryption
+			aes_ciphertext = aes_encrypt(msg);
+			//System.out.println("encrypted file: " + toHexString(aes_ciphertext));
+			out_file.write(aes_ciphertext);
+			out_file.close();
 
         }catch (Exception e){
 
         }finally{
-            
+
         }
 
         System.out.println("secureFile worked");
     }
+
+
+    public static byte[] sha1_hash(byte[] input_data) throws Exception{
+		byte[] hashval = null;
+		try{
+			//create message digest object
+			MessageDigest sha1 = MessageDigest.getInstance("SHA1");
+			
+			//make message digest
+			hashval = sha1.digest(input_data);
+		}
+		catch(NoSuchAlgorithmException nsae){
+			System.out.println(nsae);
+		}
+		return hashval;
+    }
+    
+    public static byte[] aes_encrypt(byte[] data_in) throws Exception{
+		byte[] out_bytes = null;
+		try{
+			//set cipher object to encrypt mode
+			sec_cipher.init(Cipher.ENCRYPT_MODE, sec_key_spec);
+
+			//create ciphertext
+			out_bytes = sec_cipher.doFinal(data_in);
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+		return out_bytes;
+    }
+    
+
+    /*
+     * Converts a byte array to hex string
+     * this code from http://java.sun.com/j2se/1.4.2/docs/guide/security/jce/JCERefGuide.html#HmacEx
+     */
+    public static String toHexString(byte[] block) {
+        StringBuffer buf = new StringBuffer();
+
+        int len = block.length;
+
+        for (int i = 0; i < len; i++) {
+             byte2hex(block[i], buf);
+             if (i < len-1) {
+                 buf.append(":");
+             }
+        } 
+        return buf.toString();
+    }
+    /*
+     * Converts a byte to hex digit and writes to the supplied buffer
+     * this code from http://java.sun.com/j2se/1.4.2/docs/guide/security/jce/JCERefGuide.html#HmacEx
+     */
+    public static void byte2hex(byte b, StringBuffer buf) {
+        char[] hexChars = { '0', '1', '2', '3', '4', '5', '6', '7', '8',
+                            '9', 'A', 'B', 'C', 'D', 'E', 'F' };
+        int high = ((b & 0xf0) >> 4);
+        int low = (b & 0x0f);
+        buf.append(hexChars[high]);
+        buf.append(hexChars[low]);
+    }
+
 }
