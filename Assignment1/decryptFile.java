41ec323eefa9675cf6dab7bbe2bc1b0ccdc8cdb4
Hanum Lee <daniel@Hanums-MacBook-Pro.local>
2018-10-08 20:12:30 -0600

    basic structure added for secure and decrypt

---
+++ Assignment1/decryptFile.java
@@ -0,0 +1,57 @@
+
+import java.io.*;
+import java.security.*;
+import javax.crypto.*;
+import javax.crypto.spec.*;
+import java.security.interfaces.*;
+import java.security.interfaces.DSAKey;
+import java.math.*;
+import java.security.SecureRandom;
+
+
+
+public class decryptFile{
+    private static KeyGenerator key_gen = null;
+	private static SecretKey sec_key = null;
+	private static byte[] raw = null;
+	private static SecretKeySpec sec_key_spec = null;
+	private static Cipher sec_cipher = null;
+
+	//for DSA
+	private static KeyPairGenerator keypairgen = null;
+	private static KeyPair keypair = null;
+	private static DSAPrivateKey private_key = null;
+	private static DSAPublicKey public_key = null;
+	private static Signature dsa_sig = null;
+	private static SecureRandom secRan = null;
+    private static BigInteger big_sig = null;
+    
+    public static void main(String[] args) {
+        FileInputStream in_file = null;
+		FileInputStream in_file2 = null;
+		FileOutputStream out_file = null;
+		byte[] sha_hash = null;
+		byte[] hmac_hash = null;
+		byte[] aes_ciphertext = null;
+		byte[] sig = null;
+		String decrypted_str = new String();
+		int read_bytes = 0;
+        boolean verify = false;
+        
+        try{
+            in_file = new FileInputStream(args[0]);
+            out_file = new FileOutputStream(args[1]);
+            
+            byte[] msg = new byte[in_file.available()];
+			read_bytes = in_file.read(msg);
+
+        }catch (Exception e){
+
+        }finally{
+            
+        }
+
+
+        System.out.println("decrypt worked");
+    }
+}
